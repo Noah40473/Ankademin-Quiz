fea382fb8b2f2e36ba281c503e7727490d434646
Noah Lofland <noahsmejl@gmail.com>
2022-10-24 15:54:09 +0200

    hej

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 var myQuestions = [
 	{
-		question: "What is 10/2?",
+		question: "Är Jorden plat?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '115'
+			a: 'Ja',
+			b: 'Nej',
 		},
 		correctAnswer: 'b'
 	},
 	{
-		question: "What is 30/3?",
+		question: "Är Iphone bättre än Android?",
 		answers: {
-			a: '3',
-			b: '5',
-			c: '10'
+			a: 'Ja',
+			b: 'Nej',
 		},
-		correctAnswer: 'c'
+		correctAnswer: 'a'
 	},
 	{
 		question: "What is 10/2?",
@@ -108,20 +106,20 @@ generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
 function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 
 	function showQuestions(questions, quizContainer){
-		// we'll need a place to store the output and the answer choices
+		// vi behöver en plats för att lagra output och svarsalternativ
 		var output = [];
 		var answers;
 
-		// for each question...
+		// för varje fråga...
 		for(var i=0; i<questions.length; i++){
 			
-			// first reset the list of answers
+			// återställ först listan med svar
 			answers = [];
 
-			// for each available answer...
+			// för varje tillgängligt svar...
 			for(letter in questions[i].answers){
 
-				// ...add an html radio button
+				// ...lägg till en html-radioknapp
 				answers.push(
 					'<label>'
 						+ '<input type="radio" name="question'+i+'" value="'+letter+'">'
@@ -131,56 +129,57 @@ function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
 				);
 			}
 
-			// add this question and its answers to the output
+			// lägg till denna fråga och dess svar till utdata
 			output.push(
 				'<div class="question">' + questions[i].question + '</div>'
 				+ '<div class="answers">' + answers.join('') + '</div>'
 			);
 		}
 
-		// finally combine our output list into one string of html and put it on the page
+		// kombinera slutligen vår utdatalista till en sträng av html och lägg den på sidan
 		quizContainer.innerHTML = output.join('');
 	}
 
 
 	function showResults(questions, quizContainer, resultsContainer){
 		
-		// gather answer containers from our quiz
+		// samla svarsbehållare från vårt frågesport
 		var answerContainers = quizContainer.querySelectorAll('.answers');
 		
-		// keep track of user's answers
+		// hålla reda på användarens svar
 		var userAnswer = '';
 		var numCorrect = 0;
 		
-		// for each question...
+		// för varje fråga...
 		for(var i=0; i<questions.length; i++){
 
-			// find selected answer
+			// hitta valt svar
 			userAnswer = (answerContainers[i].querySelector('input[name=question'+i+']:checked')||{}).value;
 			
-			// if answer is correct
+			// om svaret är rätt
 			if(userAnswer===questions[i].correctAnswer){
-				// add to the number of correct answers
+				// lägg till antalet korrekta svar
 				numCorrect++;
 				
-				// color the answers green
+				// färga svaren gröna
 				answerContainers[i].style.color = 'lightgreen';
 			}
-			// if answer is wrong or blank
+			// om svaret är fel eller tomt
 			else{
-				// color the answers red
+				// färga svaren röda
 				answerContainers[i].style.color = 'red';
 			}
+			
 		}
 
-		// show number of correct answers out of total
+		// visa antalet rätta svar av totalt
 		resultsContainer.innerHTML = numCorrect + ' out of ' + questions.length;
 	}
 
-	// show questions right away
+	// visa frågor direkt
 	showQuestions(questions, quizContainer);
 	
-	// on submit, show results
+	// vid inlämning, visa resultat
 	submitButton.onclick = function(){
 		showResults(questions, quizContainer, resultsContainer);
 	}
